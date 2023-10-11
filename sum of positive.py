dfc5078962a18446a5d3ee6c079ea9ee4359f451
JohnDunthorne <johndunthorne1@gmail.com>
2023-10-11 04:56:09 -0500

    1

--- sum of positive.py
+++ sum of positive.py
@@ -1,7 +1,17 @@
-n = 0
+n = int(input("integer: "))
 y = 1
 while y < n + 1:
     print(f"{y} sheep...", end="")
     y += 1
 else:
     print("")
+
+
+# like this
+
+
+def count_sheep(n):
+    result = ""
+    for number in range(1, n + 1):
+        result = result + str(number) + " sheep..."
+    return result
