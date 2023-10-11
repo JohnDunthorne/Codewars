63f3c287cebc1df4ca5f8c72ab8dc0a436ba7fef
JohnDunthorne <johndunthorne1@gmail.com>
2023-10-11 17:15:48 -0500

    1

--- booltoword.py
+++ booltoword.py
@@ -3,3 +3,15 @@ def bool_to_word(boolean):
         return "Yes"
     else:
         return "No"
+
+
+import codewars_test as test
+from solution import bool_to_word
+
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it("Basic Test Cases")
+    def basic_test_cases():
+        test.assert_equals(bool_to_word(True), "Yes")
+        test.assert_equals(bool_to_word(False), "No")
